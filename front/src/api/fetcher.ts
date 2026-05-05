1591e0f8dba69fd9b5abce85c216c072d498b8d9
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-05-05 17:11:56 +0900

    refactor: API fetcher 공통 로직 구조 개선

--- front/src/api/fetcher.ts
+++ front/src/api/fetcher.ts
@@ -1,17 +1,22 @@
-const BASE_URL = "https://apis.data.go.kr/B551011/KorService2";
+const BASE_URL = "https://apis.data.go.kr";
 
-export const fetcher = async (
-  endpoint: string,
-  params?: Record<string, string>,
-) => {
+const DEFAULT_PARAMS = {
+  serviceKey: process.env.NEXT_PUBLIC_DECODING_AUTH_KEY as string,
+  MobileOS: "ETC",
+  MobileApp: "MyApp",
+  _type: "json",
+};
+
+export const fetcher = async <T>(
+  path: string,
+  params?: Record<string, string | number>,
+): Promise<T> => {
   const searchParams = new URLSearchParams({
-    serviceKey: process.env.NEXT_PUBLIC_DECODING_AUTH_KEY as string,
-    MobileOS: "ETC",
-    MobileApp: "MyApp",
-    _type: "json",
-    ...params,
+    ...DEFAULT_PARAMS,
+    ...(params as Record<string, string>),
   });
-  const response = await fetch(`${BASE_URL}${endpoint}?${searchParams}`);
+
+  const response = await fetch(`${BASE_URL}${path}?${searchParams}`);
   if (!response.ok) throw new Error(`HTTP error! ${response.status}`);
   return response.json();
 };
